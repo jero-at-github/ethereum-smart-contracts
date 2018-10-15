0696cb55e2d4ec9a518d8c8a23f1130d64d566aa
Jero <jlopeznavarro@gmail.com>
2018-10-15 19:26:19 +0200

    Updated truffle config files

--- smart_contracts/truffle-config.js
+++ smart_contracts/truffle-config.js
@@ -12,7 +12,26 @@
  *   },
  */
 
+var HDWalletProvider = require('truffle-hdwallet-provider');
+
+var mnemonic = 'embark clarify seven matter sauce then poet math hammer recycle business social';
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {    
+    development: {
+      host: "localhost",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+        provider: function() { 
+            return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/8da2ccf1f93e4660a6a9d1fabbf4ffd0') 
+        },
+        network_id: 4,
+        gas: 4500000,
+        gasPrice: 10000000000,
+    }    
+  }
 };
